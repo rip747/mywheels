rip747/mywheels

commit 68df8e4182224bafa59e56a9ba06f6087ba44d57

match and purge whitespace in where clause fixes issue 381
git-svn-id: https://cfwheels.googlecode.com/svn/trunk@4007 d8ff095c-9719-0410-9cd3-1dd5d13d90f5

wheels/model/initialization.cfm
@@ -6,8 +6,8 @@
 		variables.wheels.errors = [];
 		variables.wheels.class = {};
 		variables.wheels.class.modelName = arguments.name;
-		variables.wheels.class.RESQLOperators = "((?: LIKE)|(?:<>)|(?:<=)|(?:>=)|(?:!=)|(?:!<)|(?:!>)|=|<|>)";
-		variables.wheels.class.RESQLWhere = "(#variables.wheels.class.RESQLOperators# ?)(''|'.+?'()|(-?[0-9]|\.)+()|\(-?[0-9]+(,-?[0-9]+)*\))(($|\)| (AND|OR)))";
+		variables.wheels.class.RESQLOperators = "[[:space:]]*((?:LIKE)|(?:IN)|(?:<>)|(?:<=)|(?:>=)|(?:!=)|(?:!<)|(?:!>)|=|<|>)";
+		variables.wheels.class.RESQLWhere = "(#variables.wheels.class.RESQLOperators#[[:space:]]*)(''|'.+?'()|(-?[0-9]|\.)+()|\(-?[0-9]+(,-?[0-9]+)*\))(($|\)|[[:space:]]*(AND|OR)))";
 		variables.wheels.class.mapping = {};
 		variables.wheels.class.properties = {};
 		variables.wheels.class.calculatedProperties = {};

wheels/tests/model/crud/where.cfc
@@ -1,0 +1,17 @@
+<cfcomponent extends="wheelsMapping.test">
+
+	<cfinclude template="/wheelsMapping/global/functions.cfm">
+	
+	<cffunction name="test_contains_multiple_spaces_tabs_and_carriage_returns">
+		<cfset loc.r = model("post").findAll(where="views 
+				
+					 >= 			
+					   5   		   AND  
+		  averagerating     
+		   >   
+		   				3")>
+		<cfset loc.e = loc.r['authorid'][1]>
+		<cfset assert('loc.e eq 9')>
+	</cffunction>
+	
+</cfcomponent>

wheels/model/crud.cfm
@@ -1136,7 +1136,7 @@
 				else
 					loc.elementDataPart = loc.element;
 				loc.elementDataPart = Trim(ReplaceList(loc.elementDataPart, "AND,OR", ""));
-				loc.temp = REFind("^([a-zA-Z0-9-_\.]*) ?#variables.wheels.class.RESQLOperators#", loc.elementDataPart, 1, true);
+				loc.temp = REFind("^([a-zA-Z0-9-_\.]*)#variables.wheels.class.RESQLOperators#", loc.elementDataPart, 1, true);
 				if (ArrayLen(loc.temp.len) > 1)
 				{
 					loc.where = Replace(loc.where, loc.element, Replace(loc.element, loc.elementDataPart, "?", "one"));
@@ -1165,7 +1165,7 @@
 					}
 					if (application.wheels.showErrorInformation && !StructKeyExists(loc.param, "column"))
 						$throw(type="Wheels.ColumnNotFound", message="Wheels looked for the column mapped to the `#loc.param.property#` property but couldn't find it in the database table.", extendedInfo="Verify the `where` argument and/or your property to column mappings done with the `property` method inside the model's `init` method to make sure everything is correct.");
-					loc.temp = REFind("^[a-zA-Z0-9-_\.]* ?#variables.wheels.class.RESQLOperators#", loc.elementDataPart, 1, true);
+					loc.temp = REFind("^[a-zA-Z0-9-_\.]*#variables.wheels.class.RESQLOperators#", loc.elementDataPart, 1, true);
 					loc.param.operator = Trim(Mid(loc.elementDataPart, loc.temp.pos[2], loc.temp.len[2]));
 					ArrayAppend(loc.params, loc.param);
 				}

commit 3464a6f468ce43e6f6962392c9c6e573a713afd6

fixed regression in r4007 in statement broke supplied test was unreliable
git-svn-id: https://cfwheels.googlecode.com/svn/trunk@4008 d8ff095c-9719-0410-9cd3-1dd5d13d90f5

wheels/model/initialization.cfm
@@ -6,7 +6,7 @@
 		variables.wheels.errors = [];
 		variables.wheels.class = {};
 		variables.wheels.class.modelName = arguments.name;
-		variables.wheels.class.RESQLOperators = "[[:space:]]*((?:LIKE)|(?:IN)|(?:<>)|(?:<=)|(?:>=)|(?:!=)|(?:!<)|(?:!>)|=|<|>)";
+		variables.wheels.class.RESQLOperators = "[[:space:]]*((?:LIKE)|(?:<>)|(?:<=)|(?:>=)|(?:!=)|(?:!<)|(?:!>)|=|<|>)";
 		variables.wheels.class.RESQLWhere = "(#variables.wheels.class.RESQLOperators#[[:space:]]*)(''|'.+?'()|(-?[0-9]|\.)+()|\(-?[0-9]+(,-?[0-9]+)*\))(($|\)|[[:space:]]*(AND|OR)))";
 		variables.wheels.class.mapping = {};
 		variables.wheels.class.properties = {};

wheels/tests/model/crud/where.cfc
@@ -1,17 +1,21 @@
 <cfcomponent extends="wheelsMapping.test">
 
 	<cfinclude template="/wheelsMapping/global/functions.cfm">
-	
+
 	<cffunction name="test_contains_multiple_spaces_tabs_and_carriage_returns">
-		<cfset loc.r = model("post").findAll(where="views 
-				
-					 >= 			
-					   5   		   AND  
-		  averagerating     
-		   >   
+		<cfset loc.r = model("post").findAll(where="views
+
+					 >=
+					   5   		   AND
+		  averagerating
+		   >
 		   				3")>
-		<cfset loc.e = loc.r['authorid'][1]>
-		<cfset assert('loc.e eq 9')>
+		<cfset assert('loc.r.recordcount eq 1')>
 	</cffunction>
-	
+
+	<cffunction name="test_in_statment_should_not_error">
+		<cfset loc.r = model("post").findAll(where="views IN(5,3)")>
+		<cfset assert('loc.r.recordcount eq 3')>
+	</cffunction>
+
 </cfcomponent>